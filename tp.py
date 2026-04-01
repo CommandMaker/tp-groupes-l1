a9bfd393c5c2ca094022209f8d8a08da20c9ac11
TOULON ETHAN <etoulon869@u1-004-15.univ-tln.fr>
2026-04-01 18:06:19 +0200

    feat: fin du TP

--- tp.py
+++ tp.py
@@ -1,6 +1,10 @@
 # Fonctions données dans l'énoncé
+import math
+
 
 type Permutation = list[int]
+type TCycle = tuple[int, ...]
+type Transposition = tuple[int, int]
 
 # Lecture d'une permutation
 def Lire(texte: str = "s = ") -> Permutation:
@@ -52,4 +56,60 @@ def Composer(t: Permutation, s: Permutation) -> Permutation:
     return compose
 
 
-print(Composer([1, 4, 3, 2, 0, 5], [2, 4, 1, 5, 0, 3]))
+def Orbite(k: int, s: Permutation) -> list[int]:
+    orbite: list[int] = []
+
+    last_value: int = s[k]
+    orbite += [k]
+
+    while last_value != k:
+        orbite += [last_value]
+        last_value = s[last_value]
+
+    return orbite
+
+
+def Cycle(s: Permutation) -> list[TCycle]:
+    l: list[TCycle] = []
+    B: list[bool] = [False] * len(s)
+
+    for i in range(len(s)):
+        if not B[i]:
+            orbite = Orbite(i, s)
+
+            if len(orbite) > 1:
+                cycle: tuple[int, ...] = tuple()
+
+                while len(orbite) > 0:
+                    element = orbite.pop(0)
+                    B[element] = True
+                    cycle += (element,)
+
+                l += [cycle]
+
+    return l
+
+
+def Transpositions(s: Permutation) -> list[Transposition]:
+    l: list[Transposition] = []
+    cycles = Cycle(s)
+
+    for cycle in cycles:
+        buffer = tuple()
+
+        for item in cycle:
+            buffer += (item,)
+
+            if len(buffer) == 2:
+                l += [buffer]
+                buffer = (item,)
+
+    return l
+
+
+def Signature(s: Permutation) -> int:
+    return (-1) ** len(Transpositions(s)) # pyright: ignore[reportAny]
+
+t = [3, 5, 1, 2, 0, 4, 6]
+print(Cycle(t))
+print(Transpositions(t))
